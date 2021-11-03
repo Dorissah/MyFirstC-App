5411898e05ad3325180019456388170112bd8b2e
Dorissah <dorisagonsi@gmail.com>
2021-11-03 08:34:32 +0100

    Bank App

--- Csharp Assignment1/Models/SavingsAccount.cs
+++ Csharp Assignment1/Models/SavingsAccount.cs
@@ -8,5 +8,23 @@ namespace Csharp_Assignment1.Models
 {
     class SavingsAccount
     {
+            public string AccountName { get; }
+            public string AccountNumber { get; }
+            private int BVN { get; }
+
+            string checkBalance(int AccountNumber, int AccountBalance)
+            {
+                return ("Your Account Balance ");
+            }
+            string transferMoney(string AccountNumber, string AccountOwner, int Amount)
+            {
+                string transferDetails = AccountNumber + AccountOwner + Amount;
+                return transferDetails;
+            }
+
+            void recieveMoney()
+            {
+
+            }
     }
 }

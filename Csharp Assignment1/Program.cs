df4220591470e6ec3bad752707d0aa9fe2d3c564
Dorissah <dorisagonsi@gmail.com>
2021-11-03 08:28:04 +0100

    Bank App

---
+++ Csharp Assignment1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
